ddf2c71efb9ef50b6553e457a5f7dec45bc1dd1b
marlonricardo <mroparaty@gmail.com>
2023-09-28 22:18:30 -0300

    Add Pattern Matching for instanceof

--- src/com/mro/javainsider/application/App3.java
+++ src/com/mro/javainsider/application/App3.java
@@ -17,8 +17,10 @@ public class App3 {
         //casting explicito, tomar cuidado pois pode esta fazendo casting explicito
         // para um tipo que não está na memória, no caso, Fish f = (Fish) p;
 
-        if(p instanceof Dog){
-            Dog d = (Dog) p;
+        //Pattern Matching for instanceof - basicamente já cria a variável, caso a condicao for true
+        //Pattern matching enables you to remove the conversion step by changing the second operand of
+        // the instanceof operator with a type pattern, making your code shorter and easier to read
+        if(p instanceof Dog d){
             d.sit();
         }else{
             System.out.println("O tipo não é um dog!");
